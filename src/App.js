cf0606858752b0619c1186e5920b2360f0490647
saugat977 <jungsaugat@gmail.com>
2021-03-18 15:26:11 +0545

    react commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First';
+import Second from './Second';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h1>Hello World</h1> */}
+     {/* jsx syntax */}
+     {/* <First/>  */}
+     {/* <Second/> */}
+     <Navbar/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Second.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Second=()=> {
+    return (
+        <>
+         <p>this is written on second component</p>   
+        </>
+    )
+}
+
+export default Second
